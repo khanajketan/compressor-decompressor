60c15bb5e32b91035f003f9f856d65a34eaaa86c
Ketan Khanaj <khanajketan@gmail.com>
2023-04-23 12:33:08 +0530

    1st comp-decomp commit

---
+++ src/main/java/ComDcom/COMPRESSOR_DECOMPRESSOR.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ComDcom;
+
+/**
+ *
+ * @author Ketan
+ */
+
+
+public class COMPRESSOR_DECOMPRESSOR {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
